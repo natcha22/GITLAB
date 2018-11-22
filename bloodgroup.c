746e163ab3a08ba08f9f5a65981c150d4d7e5f0b
tamjtm <dreamgoom.i@gmail.com>
2018-11-22 16:33:58 +0700

    add program #3

--- bloodgroup.c
+++ bloodgroup.c
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye. Wish you good luck!\n");
+  printf("Goodbye. Have a nice !\n");
 }
